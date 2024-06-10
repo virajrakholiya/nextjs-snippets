7c72c9cb5b015575e6a42202112e158e0f3e1994
Viraj Rakholiya <virajrakholiya14@gmail.com>
2024-06-10 20:33:07 +0530

    commit

--- src/app/snippets/[id]/loading.tsx
+++ src/app/snippets/[id]/loading.tsx
@@ -4,7 +4,7 @@ function loading() {
   return (
     //bcz we are on local page move fast use this for delay on page.tsx ==  await new Promise(r=> setTimeout(r,2000))
 
-    <div>Loading...</div>
+    <div>Loading....</div>
   )
 }
 
