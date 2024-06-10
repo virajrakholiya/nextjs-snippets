ec92b0f7a7961fa13ca8c04b37a2d5f9fab1fb2c
Viraj Rakholiya <virajrakholiya14@gmail.com>
2024-06-10 20:26:33 +0530

    commit

--- src/components/snippetEditForm.tsx
+++ src/components/snippetEditForm.tsx
@@ -3,23 +3,27 @@ import { Editor } from "@monaco-editor/react";
 import type { Snippet } from "@prisma/client";
 import { useState } from "react";
 import { editSnippet } from "@/actions/action";
+
 interface SnippetsEditForm {
   snippet: Snippet;
 }
 
-function snippetEditFrom({ snippet }: SnippetsEditForm) {
+function SnippetEditForm({ snippet }: SnippetsEditForm) {
   const [code, setCode] = useState(snippet.code);
-  const HandelEditorChange = (value: string = "") => {
+
+  const handleEditorChange = (value: string = "") => {
     setCode(value);
   };
+
   const action = editSnippet.bind(null, snippet.id, code);
+
   return (
     <div className="mt-5">
-      <div className="h-8 bg-gray-900 rounded-t-xl p-2 ">
+      <div className="h-8 bg-gray-900 rounded-t-xl p-2">
         <div className="ml-3 flex gap-3 items-center">
-          <div className="bg-red-500 h-4  rounded-full w-4"></div>
-          <div className="bg-yellow-500 h-4  rounded-full w-4"></div>
-          <div className="bg-green-500 h-4  rounded-full w-4"></div>
+          <div className="bg-red-500 h-4 rounded-full w-4"></div>
+          <div className="bg-yellow-500 h-4 rounded-full w-4"></div>
+          <div className="bg-green-500 h-4 rounded-full w-4"></div>
         </div>
       </div>
       <Editor
@@ -34,15 +38,14 @@ function snippetEditFrom({ snippet }: SnippetsEditForm) {
           },
           fontSize: 18,
         }}
-        onChange={HandelEditorChange}
+        onChange={handleEditorChange}
         className="bg-gray-900"
-
       />
-      <div className="h-8 bg-gray-900 rounded-b-xl "></div>
+      <div className="h-8 bg-gray-900 rounded-b-xl"></div>
       <form action={action} className="mt-5">
         <button
           type="submit"
-          className="btn p-2 border  rounded-xl bg-black text-white"
+          className="btn p-2 border rounded-xl bg-black text-white"
         >
           Save
         </button>
@@ -51,4 +54,4 @@ function snippetEditFrom({ snippet }: SnippetsEditForm) {
   );
 }
 
-export default snippetEditFrom;
+export default SnippetEditForm;
